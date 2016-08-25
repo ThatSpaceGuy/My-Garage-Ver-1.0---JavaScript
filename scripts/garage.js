6b702a15289ded9ba0480b520ec20d677547feb0
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-25 12:58:58 -0500

    Basic User Input Retrieval - ver 0.2

--- scripts/garage.js
+++ scripts/garage.js
@@ -1,1 +1,14 @@
 console.log('garage.js sourced!');
+
+var addCar = function (){
+  console.log('in addCar');
+  // get user input
+  var carYear = document.getElementById('yearIn').value;
+  var carMake = document.getElementById('makeIn').value;
+  var carModel = document.getElementById('modeIn').value;
+  var carPicURL = document.getElementById('picURLIn').value;
+  var carDescription = document.getElementById('descriptionIn').value;
+  // create car object
+  // push car into garage
+  // display cars
+}; // end addCar
