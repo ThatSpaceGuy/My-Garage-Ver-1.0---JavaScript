8cc8d7956f6de5384d1e8b52627aed6f09f07eee
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-25 13:05:43 -0500

    Create Car Object from User input - ver 0.3

--- scripts/garage.js
+++ scripts/garage.js
@@ -3,12 +3,15 @@ console.log('garage.js sourced!');
 var addCar = function (){
   console.log('in addCar');
   // get user input
-  var carYear = document.getElementById('yearIn').value;
-  var carMake = document.getElementById('makeIn').value;
-  var carModel = document.getElementById('modeIn').value;
-  var carPicURL = document.getElementById('picURLIn').value;
-  var carDescription = document.getElementById('descriptionIn').value;
   // create car object
+  var newCar = {
+    year: document.getElementById('yearIn').value,
+    make: document.getElementById('makeIn').value,
+    model: document.getElementById('modeIn').value,
+    picURL: document.getElementById('picURLIn').value,
+    description: document.getElementById('descriptionIn').value
+  };
+  console.log(newCar);
   // push car into garage
   // display cars
 }; // end addCar
