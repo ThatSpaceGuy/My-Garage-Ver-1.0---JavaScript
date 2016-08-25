972e3ffcd8d08d65b200f02f456f3867d0a90e73
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-25 13:56:44 -0500

    List garage cars on the DOM - ver 0.5

--- scripts/garage.js
+++ scripts/garage.js
@@ -9,7 +9,7 @@ var addCar = function (){
   var newCar = {
     year: document.getElementById('yearIn').value,
     make: document.getElementById('makeIn').value,
-    model: document.getElementById('modeIn').value,
+    model: document.getElementById('modelIn').value,
     picURL: document.getElementById('picURLIn').value,
     description: document.getElementById('descriptionIn').value
   };
@@ -23,4 +23,24 @@ var addCar = function (){
 var displayCars = function () {
   console.log('in displayCars');
   console.log(garage);
+
+  carList = document.getElementById('allCars');
+  // empty our ul element
+  carList.innerHTML='';
+
+  // for each car in the garage, add a list item with year, make and model
+  for (var i = 0; i < garage.length; i++) {
+    // basic car info - year, make, model
+    var carInfo = garage[i].year+' '+garage[i].make+' '+garage[i].model;
+    // put carInfo inside a list item through a textNode
+    // create the li
+    var listItem = document.createElement('li');
+    // create the textNode
+    var textNode = document.createTextNode(carInfo);
+    // append textNode as child to li
+    listItem.appendChild(textNode);
+    // append list item to the ul
+    carList.appendChild(listItem);
+  }
+
 };
