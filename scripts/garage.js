b3cba474abae60c973ae2f8ee35dba439401b5e9
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-26 16:52:42 -0500

    Add sellCar and driveCar functionality and styling - ver 1.0

--- scripts/garage.js
+++ scripts/garage.js
@@ -11,7 +11,8 @@ var addCar = function (){
     make: document.getElementById('makeIn').value,
     model: document.getElementById('modelIn').value,
     picURL: document.getElementById('picURLIn').value,
-    description: document.getElementById('descriptionIn').value
+    description: document.getElementById('descriptionIn').value,
+    driving: false
   };
 
   if ( newCar.year === '' || newCar.make === '' || newCar.model === ''){
@@ -42,12 +43,24 @@ var displayCars = function () {
   // empty our ul element
   carList.innerHTML='';
 
+  if (garage.length === 0){
+    carList.innerHTML='<h2>Garage is Empty!</h2>';
+  }
   // for each car in the garage, add a list item with year, make and model
   for (var i = 0; i < garage.length; i++) {
+    var buttonText='Drive';
+    var picToShow=garage[i].picURL;
+    var descText=garage[i].description;
+    if (garage[i].driving){
+      buttonText='Return';
+      picToShow='https://thumbs.dreamstime.com/x/empty-garage-21054151.jpg';
+      descText='This car is currently out for a drive.';
+    }
     // basic car info - year, make, model
     var carInfo = '<h2>'+garage[i].year+' '+garage[i].make+' '+garage[i].model+
-    '</h2><img src="'+garage[i].picURL+'"><p>'+garage[i].description+
-    ' <button onClick="takeOut('+i+')">Take me out for a drive</button></p>';
+    '</h2><img src="'+picToShow+'"><p>'+descText+
+    ' <button onClick="sellCar('+i+')">Sell this car!</button>'+
+    ' <button onClick="driveCar('+i+')">'+buttonText+' this car!</button></p>';
     // append car info to output div
     carList.innerHTML += carInfo;
 
@@ -55,10 +68,19 @@ var displayCars = function () {
 
 };
 
-var takeOut = function(index){
-  console.log('in takeOut with index:',index);
+var driveCar = function(index){
+  console.log('in driveCar with index:',index);
 
-  console.log('taking out the '+garage[index].year+' '+garage[index].model);
+  garage[index].driving = !garage[index].driving;
+
+  displayCars();
+};
+
+var sellCar = function(index){
+  console.log('in sellCar with index:',index);
+
+  alert('Selling the '+garage[index].year+' '+garage[index].model+
+  '... It will no longer appear in the garage.');
   // splice the car out of the array
   garage.splice(index,1);
   // show cars in garage again
