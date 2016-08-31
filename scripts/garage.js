1727ffe29959f5e80f4c0a017ad294e02dc8925d
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-30 21:28:12 -0500

    First sweep at jQuerifying Garage

--- scripts/garage.js
+++ scripts/garage.js
@@ -7,11 +7,11 @@ var addCar = function (){
   // get user input
   // create car object
   var newCar = {
-    year: document.getElementById('yearIn').value,
-    make: document.getElementById('makeIn').value,
-    model: document.getElementById('modelIn').value,
-    picURL: document.getElementById('picURLIn').value,
-    description: document.getElementById('descriptionIn').value,
+    year: $('#yearIn').val(),
+    make: $('#makeIn').val(),
+    model: $('#modelIn').val(),
+    picURL: $('#picURLIn').val(),
+    description: $('#descriptionIn').val(),
     driving: false
   };
 
@@ -28,44 +28,44 @@ var addCar = function (){
 
 var clearInputs = function(){
   console.log('in clearInputs');
-  document.getElementById('yearIn').value = '';
-  document.getElementById('makeIn').value = '';
-  document.getElementById('modelIn').value = '';
-  document.getElementById('picURLIn').value = '';
-  document.getElementById('descriptionIn').value = '';
+  $('#yearIn').val('');
+  $('#makeIn').val('');
+  $('#modelIn').val('');
+  $('#picURLIn').val('');
+  $('#descriptionIn').val('');
 };
 
 var displayCars = function () {
   console.log('in displayCars');
   console.log(garage);
 
-  carList = document.getElementById('allCars');
+  carList = $('#allCars');
   // empty our ul element
-  carList.innerHTML='';
+  carList.html('');
 
   if (garage.length === 0){
-    carList.innerHTML='<h2>Garage is Empty!</h2>';
-  }
-  // for each car in the garage, add a list item with year, make and model
-  for (var i = 0; i < garage.length; i++) {
-    var buttonText='Drive';
-    var picToShow=garage[i].picURL;
-    var descText=garage[i].description;
-    if (garage[i].driving){
-      buttonText='Return';
-      picToShow='https://thumbs.dreamstime.com/x/empty-garage-21054151.jpg';
-      descText='This car is currently out for a drive.';
+    carList.html('<h2>Garage is Empty!</h2>');
+  } else {
+    var garageFloor = '';
+    // for each car in the garage, add a list item with year, make and model
+    for (var i = 0; i < garage.length; i++) {
+      var buttonText='Drive';
+      var picToShow=garage[i].picURL;
+      var descText=garage[i].description;
+      if (garage[i].driving){
+        buttonText='Return';
+        picToShow='https://thumbs.dreamstime.com/x/empty-garage-21054151.jpg';
+        descText='This car is currently out for a drive.';
+      }
+      // basic car info - year, make, model
+      garageFloor += '<h2>'+garage[i].year+' '+garage[i].make+' '+garage[i].model+
+      '</h2><img src="'+picToShow+'"><p>'+descText+
+      ' <button onClick="sellCar('+i+')">Sell this car!</button>'+
+      ' <button onClick="driveCar('+i+')">'+buttonText+' this car!</button></p>';
     }
-    // basic car info - year, make, model
-    var carInfo = '<h2>'+garage[i].year+' '+garage[i].make+' '+garage[i].model+
-    '</h2><img src="'+picToShow+'"><p>'+descText+
-    ' <button onClick="sellCar('+i+')">Sell this car!</button>'+
-    ' <button onClick="driveCar('+i+')">'+buttonText+' this car!</button></p>';
     // append car info to output div
-    carList.innerHTML += carInfo;
-
+    carList.html(garageFloor);
   }
-
 };
 
 var driveCar = function(index){
